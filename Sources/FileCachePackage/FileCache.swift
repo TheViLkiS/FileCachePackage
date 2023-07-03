625b2dc5634ce819851d97d55c8a6f5b44923bed
TheViLkiS <thevilkis@gmail.com>
2023-07-03 12:03:31 +0400

    1

--- Sources/FileCachePackage/FileCache.swift
+++ Sources/FileCachePackage/FileCache.swift
@@ -169,7 +169,7 @@ extension FileCache {
                 try? fileCache.addNewToDo(i)
             }
             
-            fileCache.saveToFile(fileName: "fileCacheForTestsTwo", fileType: .json)
+            fileCache.saveToFile(fileName: "fileCacheForTests", fileType: .json)
         }
     }
 }
